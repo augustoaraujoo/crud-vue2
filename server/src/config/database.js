2bae7776b75d4a7ae03cf090b3c49d9c3006b790
augustoroocha <augustinho_darocha1311@hotmail.com>
2021-10-12 14:44:54 -0300

    desenvolvimento .env api

---
+++ server/src/config/database.js
@@ -0,0 +1,22 @@
+/*
+    *descrição: arquivo responsável pelas 'connectrionStrings' (cosmo db e postgreSql)
+*/
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//conexão com a base de dados 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
+pool.on('error', (err, client) => {
+    console.log('Unexpected error on idle client', err);
+    process.env(-1);
+})
+pool.on('connect', () => {
+    console.log('base de dados conectado com sucesso!');
+});
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+}
