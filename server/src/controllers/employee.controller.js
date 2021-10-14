4d0e9b587711848d84df14df3ab01f95b71f5776
augustoroocha <augustinho_darocha1311@hotmail.com>
2021-10-14 12:14:39 -0300

    desenvolvendo updateEmployeeById

--- server/src/controllers/employee.controller.js
+++ server/src/controllers/employee.controller.js
@@ -43,3 +43,26 @@ exports.findEmployeeById = async (req, res) => {
     const response = await db.query('SELECT * FROM employee WHERE employee_id = $1', [employeeId]);
     res.status(200).send(response.rows);
 }
+
+// ==> Método responsável por atualizar um determinado 'Employee' por Id:
+exports.updateEmployeeById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const { name, job_role, salary, birth, employee_registration } = req.body;
+        const { rows } = await db.query(`UPDATE employee 
+                                      SET name = $1, 
+                                      job_role = $2, 
+                                      salary = $3, 
+                                      birth = $4, 
+                                      employee_registration = $5 
+                                      WHERE employee_id = $6`,
+            [name, job_role, salary, birth, employee_registration, id]
+        );
+        res.status(200).send({ message: "Employee Updated Successfully!" });
+    } catch (error) {
+        console.error('updateEmployeeById', error);
+        res.status(500).send({
+            message: "Ocorreu um erro."
+        });
+    }
+};
--- server/src/routes/employee.routes.js
+++ server/src/routes/employee.routes.js
@@ -1,19 +1,22 @@
 /*
-    *descrição: arquivo responsável pelas rotas da API
+    * descrição: arquivo responsável pelas rotas da API
 */
 const router = require('express').Router();
 /* ➡️➡️ const router = require('express-promise-router')();*/
 const employeeController = require('../controllers/employee.controller');
 
-// ==> Definindo as rotas do CRUD - 'Employee'
+// 😃 Definindo as rotas do CRUD - 'Employee'
 
-// ==> Rota responsável por criar um novo 'Colaborador(a)': (POST): localhost:3000/api/employees
+// ➡️ Rota responsável por criar um novo 'Colaborador(a)': (POST): localhost:3000/api/employees
 router.post('/employees', employeeController.createEmployee);
 
 // ➡️ rota responsável por listar todos os 'Colaboradores': (GET) localhost:3000/api/employees
 router.get('/employees', employeeController.listAllEmployees);
 
-// ==> Rota responsável por listar um determinado 'Colaborador' por Id: (GET): localhost:3000/api/employees/:id
+// ➡️ Rota responsável por listar um determinado 'Colaborador' por ID: (GET): localhost:3000/api/employees/:id
 router.get('/employees/:id', employeeController.findEmployeeById)
+
+// ==> Rota reponsável por atualizar um determinado 'Colaborador(a) por Id: (PUT): localhost:3000/api/employees/:id
+router.put('/employees/:id', employeeController.updateEmployeeById)
 
 module.exports = router;
