ee1c5f80f561708976dad137d3ba7c10aa9ebe18
augustoroocha <augustinho_darocha1311@hotmail.com>
2021-10-22 15:28:37 -0200

    updateEmployeeById

--- server-react/src/controllers/employee.controller.js
+++ server-react/src/controllers/employee.controller.js
@@ -75,6 +75,30 @@ exports.findEmployeeById = async (req, res) => {
     }
 };
 
+
+// ==> Método responsável por atualizar um determinado 'Employee' por Id:
+exports.updateEmployeeById = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const { name, job_role, salary, birth, employee_registration } = req.body;
+      const { rows } = await db.query(`UPDATE employee 
+                                      SET name = $1, 
+                                      job_role = $2, 
+                                      salary = $3, 
+                                      birth = $4, 
+                                      employee_registration = $5 
+                                      WHERE employee_id = $6`,
+        [name, job_role, salary, birth, employee_registration, id]
+      );
+      res.status(200).send({ message: "Employee Updated Successfully!" });
+    } catch (error) {
+      console.error('updateEmployeeById', error);
+      res.status(500).send({
+        message: "Ocorreu um erro."
+      });
+    }
+  };
+
 exports.deleteEmployeeById = async (req, res) => {
     const { id } = req.params
     try {
--- server-react/src/routes/employee.routes.js
+++ server-react/src/routes/employee.routes.js
@@ -5,10 +5,17 @@ const employeeController = require('../controllers/employee.controller');
 
 // ==> Rota responsável por criar um novo 'Colaborador(a)': (POST): localhost:3000/api/employees
 router.post('/employees', employeeController.createEmployee);
+
 // ==> Rota responsável por listar os 'Colaboradores(a)': (GET): localhost:3000/api/employees
 router.get('/employees', employeeController.listAllEmployees);
-// ==> Rota responsável por listar um determinado 'Colaborador' por Id: (GET): localhost:3000/api/employees/:id
-router.get('/employees/:id', employeeController.findEmployeeById)
-// ==> Rota responsável por remover um determinado 'Colaboradore(a)' por ID: (DELETE): localhost:3000/api/employees/ID
+
+// ==> Rota responsável por listar um determinado 'Colaborador' por ID: (GET): localhost:3000/api/employees/:id
+router.get('/employees/:id', employeeController.findEmployeeById);
+
+// ==> Rota responsável por atualizar um determinado 'Colaborador' por ID: (PUT): localhost:3000/api/employees/:id
+router.put('/employees/:id', employeeController.updateEmployeeById);
+
+// ==> Rota responsável por remover um determinado 'Colaboradore(a)' por ID: (DELETE): localhost:3000/api/employees/:id
 router.delete('/employees/:id', employeeController.deleteEmployeeById);
+
 module.exports = router;
